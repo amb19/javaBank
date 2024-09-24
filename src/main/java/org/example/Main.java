b65916827c761b4113226ba1c9fb1ece0b2d1889
jam <amb.test19@gmail.com>
2024-09-23 23:18:56 -0300

    added feature setup functionlity

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-<<<<<<< HEAD
         System.out.println("Welcome to JavaBank ATM!.");
-=======
-        System.out.println("Welcome to JavaBank ATM! Version control with Git.");
->>>>>>> feature-setup
     }
 }
